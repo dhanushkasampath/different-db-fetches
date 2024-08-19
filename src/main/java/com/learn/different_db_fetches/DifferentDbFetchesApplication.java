759cf5d9abe21ddd81b386245584d880059b0687
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-08-19 07:06:36 +0530

    first

---
+++ src/main/java/com/learn/different_db_fetches/DifferentDbFetchesApplication.java
@@ -0,0 +1,13 @@
+package com.learn.different_db_fetches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DifferentDbFetchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DifferentDbFetchesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/different_db_fetches/DifferentDbFetchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.different_db_fetches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DifferentDbFetchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
